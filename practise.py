5ba48b90d7906970b72d368c7126f2c3138538e3
shehab1001 <shehab.eldin.said1@gmail.com>
2026-05-15 08:38:55 +0300

    string methods

--- practise.py
+++ practise.py
@@ -18,19 +18,50 @@
 #
 # print(x)
 
-txt = "Hi, My name is shehab and i'm 25 years old"
-
-
-# for letter in txt:
-#     print(letter)
-
-
-# print("free" not in txt)
-
-# if "free" not in txt:
-#     print("Hi, Man")
+# txt = "Hi, My name is shehab and i'm 25 years old"
 #
-# print(txt[9])
+#
+# # for letter in txt:
+# #     print(letter)
+#
+#
+# # print("free" not in txt)
+#
+# # if "free" not in txt:
+# #     print("Hi, Man")
+# #
+# # print(txt[9])
+#
+#
+# # slicing
+#
+# b = "Hello, World!"
+# # print(b[-5:-2])
+# # print(b.strip())
+#
+# print(b.split(","))
 
+
+# price = 200
+# text = f"The pice is {price:.2f} dollars"
+# print(text)
+
+txt = "Shehab"
+# print(txt.capitalize())
+# print(txt.casefold())
+# print(txt.center(15))
+# print(txt.count("h"))
+# print(txt.encode())
+# print(txt.endswith("b"))
+# print(txt.startswith("S"))
+# print(txt.expandtabs(16))
+# print(txt.isdecimal())
+#
+# myDict = {"name": "John", "country": "Norway"}
+# mySeparator = ""
+#
+# x = mySeparator.join(myDict)
+#
+# print(x)
 
 
