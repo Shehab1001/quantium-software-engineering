92753113ae7f78d0d0d32554c2f51cc7a5c1a5c8
shehab1001 <shehab.eldin.said1@gmail.com>
2026-05-15 05:20:27 +0300

    strings

---
+++ practise.py
@@ -0,0 +1,36 @@
+# def greet():
+#     name = input("What's your name? ")
+#     print(f"Hello {name}")
+#
+# greet()
+
+# python Casting
+
+# x = 1
+# print(float(x))
+# y = str(x)
+# print(type(y))
+#
+# x = """
+# Hi, My name is shehab
+# 25 years old
+# """
+#
+# print(x)
+
+txt = "Hi, My name is shehab and i'm 25 years old"
+
+
+# for letter in txt:
+#     print(letter)
+
+
+# print("free" not in txt)
+
+# if "free" not in txt:
+#     print("Hi, Man")
+#
+# print(txt[9])
+
+
+
